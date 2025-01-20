7b93f3824a7860624fce02fee527a909088450af
Swizec Teller <swizec@swizec.com>
2025-01-20 09:46:27 -0800

    This seems to work now

--- app/routes/index.tsx
+++ app/routes/index.tsx
@@ -3,7 +3,7 @@ import { createServerFn } from "@tanstack/start";
 
 async function fetchAAPL() {
     const response = await fetch(
-        `https://api.polygon.io/v1/open-close/AAPL/2025-01-17?adjusted=true&apiKey=${process.env.POLYGON_API_KEY}`
+        `https://api.polygon.io/v1/open-close/AAPL/2025-01-15?adjusted=true&apiKey=${process.env.POLYGON_API_KEY}`
     );
     const json = await response.json();
     console.log(json);
@@ -24,11 +24,11 @@ export const Route = createFileRoute("/")({
 function Home() {
     const router = useRouter();
     const AAPL = Route.useLoaderData();
-    const day = "2025-01-17";
+    const day = "2025-01-15";
 
     return (
         <>
-            <h1>Hello world</h1>
+            <h1>Hello react2png</h1>
             <p>Plain ole React component in a TanStack Start router.</p>
             <p>
                 AAPL stock on {day} fetched from API 👉 ${AAPL.high}
