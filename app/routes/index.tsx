171fcc9284b3ddf30618489fc10effed701367a8
Swizec Teller <swizec@swizec.com>
2025-01-19 21:25:25 -0800

    prep for imagening

--- app/routes/index.tsx
+++ app/routes/index.tsx
@@ -2,30 +2,30 @@ import * as fs from "node:fs";
 import { createFileRoute, useRouter } from "@tanstack/react-router";
 import { createServerFn } from "@tanstack/start";
 
-const filePath = "count.txt";
+// const filePath = "count.txt";
 
-async function readCount() {
-    return parseInt(
-        await fs.promises.readFile(filePath, "utf-8").catch(() => "0")
-    );
-}
+// async function readCount() {
+//     return parseInt(
+//         await fs.promises.readFile(filePath, "utf-8").catch(() => "0")
+//     );
+// }
 
-const getCount = createServerFn({
-    method: "GET",
-}).handler(() => {
-    return readCount();
-});
+// const getCount = createServerFn({
+//     method: "GET",
+// }).handler(() => {
+//     return readCount();
+// });
 
-const updateCount = createServerFn({ method: "POST" })
-    .validator((d: number) => d)
-    .handler(async ({ data }) => {
-        const count = await readCount();
-        await fs.promises.writeFile(filePath, `${count + data}`);
-    });
+// const updateCount = createServerFn({ method: "POST" })
+//     .validator((d: number) => d)
+//     .handler(async ({ data }) => {
+//         const count = await readCount();
+//         await fs.promises.writeFile(filePath, `${count + data}`);
+//     });
 
 export const Route = createFileRoute("/")({
     component: Home,
-    loader: async () => await getCount(),
+    // loader: async () => await getCount(),
 });
 
 function Home() {
@@ -35,16 +35,7 @@ function Home() {
     return (
         <>
             <h1>Hello world</h1>
-            <button
-                type="button"
-                onClick={() => {
-                    updateCount({ data: 1 }).then(() => {
-                        router.invalidate();
-                    });
-                }}
-            >
-                Add 1 to {state}?
-            </button>
+            <p>Plain ole React component</p>
         </>
     );
 }
